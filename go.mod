55915d06fee9c42850c86f5a74f978a02ba81661
Paul Chesnais <pchesnais@linkedin.com>
2019-09-11 18:24:35 -0700

    First commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/PapaCharlie/go-progress-bar
---
+++ progress_bar.go
@@ -0,0 +1,73 @@
+package util
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Progress struct {
+	count      uint64
+	lastReport time.Time
+
+	Interval uint64
+	MaxValue uint64
+
+	lock *sync.Mutex
+}
+
+func NewProgressBar(interval, maxValue uint64) *Progress {
+	if interval <= 0 {
+		interval = 1 << 10
+	}
+
+	return &Progress{
+		lastReport: time.Now(),
+		lock:       &sync.Mutex{},
+		Interval:   interval,
+		MaxValue:   maxValue,
+	}
+}
+
+var MAGS = []string{"", "K", "M", "G", "T", "P"}
+
+func (p *Progress) Count() uint64 {
+	return atomic.LoadUint64(&p.count)
+}
+
+func (p *Progress) Inc() bool {
+	c := atomic.AddUint64(&p.count, 1)
+	if c%p.Interval == 0 || c == p.MaxValue {
+		p.lock.Lock()
+		diff := time.Since(p.lastReport)
+		p.lastReport = time.Now()
+		speed := float64(p.Interval) / diff.Seconds()
+		roundSpeed := int64(speed)
+		var mag string
+		for _, h := range MAGS {
+			if roundSpeed>>10 > 0 {
+				roundSpeed >>= 10
+			} else {
+				mag = h
+				break
+			}
+		}
+		if p.MaxValue > 0 {
+			progress := float64(c) / float64(p.MaxValue) * 100
+			eta := time.Duration(float64(p.MaxValue-c)/speed) * time.Second
+			hours := int(eta.Hours())
+			minutes := int(math.Mod(eta.Minutes(), 60))
+			seconds := int(math.Mod(eta.Seconds(), 60))
+			fmt.Printf("\rProgress: %7.3f%%, Speed: %3d %1sop/s, ETA: %02d:%02d:%02d",
+				progress, roundSpeed, mag, hours, minutes, seconds)
+		} else {
+			fmt.Printf("\rSpeed: %3d %sop/s", speed, mag)
+
+		}
+		p.lock.Unlock()
+		return true
+	}
+	return false
+}
